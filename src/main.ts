54caed9de333d18bfe314861852b38cd3e980635
Abhishek <abhishek@bluecast.tech>
2026-05-13 18:02:32 +0530

    config logger

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,16 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ConsoleLogger, Logger } from '@nestjs/common';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, {
+    logger: new ConsoleLogger({
+      prefix: 'nest-boilerplate',
+      logLevels: ['log', 'error', 'warn', 'debug', 'verbose'],
+      timestamp: true,
+      colors: true,
+    }),
+  });
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
--- src/modules/example/example.service.ts
+++ src/modules/example/example.service.ts
@@ -1,26 +1,32 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, Logger } from '@nestjs/common';
 import type { CreateExampleDto } from './dto/create-example.dto';
 import type { UpdateExampleDto } from './dto/update-example.dto';
 
 @Injectable()
 export class ExampleService {
+  private readonly logger = new Logger(ExampleService.name);
   create(_createExampleDto: CreateExampleDto) {
+    this.logger.debug('create');
     return 'This action adds a new example';
   }
 
   findAll() {
+    this.logger.debug('findAll');
     return `This action returns all example`;
   }
 
   findOne(id: number) {
+    this.logger.debug('findOne');
     return `This action returns a #${id} example`;
   }
 
   update(id: number, _updateExampleDto: UpdateExampleDto) {
+    this.logger.debug('update');
     return `This action updates a #${id} example`;
   }
 
   remove(id: number) {
+    this.logger.debug('remove');
     return `This action removes a #${id} example`;
   }
 }
