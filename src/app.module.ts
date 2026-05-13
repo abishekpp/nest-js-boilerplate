b008c2f5332a9f6ffd89e8b65daae5e41029771e
Abhishek <abhishek@bluecast.tech>
2026-05-13 14:37:21 +0530

    added example and health modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ExampleModule } from './modules/example/example.module';
+import { HealthModule } from './modules/health/health.module';
 
 @Module({
-  imports: [],
+  imports: [ExampleModule, HealthModule],
   controllers: [AppController],
   providers: [AppService],
 })
